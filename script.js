990ddb5f9b44064cceff8974fa73cf6b49057b38
rasecprogram <156724062+rasecprogram@users.noreply.github.com>
2024-03-17 23:04:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+function imprimirMensagem() {
+    console.log("Conexão feita com sucesso!");
+}
+
+// Executando a função três vezes
+imprimirMensagem();
+imprimirMensagem();
+imprimirMensagem();
 
